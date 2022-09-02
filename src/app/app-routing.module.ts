252f17eb7125bcc27f149f6c54635da8f38b3348
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-09-02 22:54:21 +0200

    welcome page with auto naviage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {  NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
 
-
+const routes: Routes = [
+  { path: '', component:WelcomeComponent },
+  {path:'login',component:LoginComponent},
+  {path:'home',component:HomeComponent}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
