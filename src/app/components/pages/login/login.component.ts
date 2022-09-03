c8e2a28b2721ef465d6fee66db045314e8789545
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-09-03 04:08:43 +0200

    login page v1

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+//Material Props
+email = new FormControl ('', [Validators.required, Validators.email]);
+hide = true;
 
-  constructor() { }
+constructor() { }
 
   ngOnInit(): void {
   }
+//form methods
+getErrorMessage() {
+  if (this.email.hasError('required')) {
+    return 'You must enter a value';
+  }
 
+  return this.email.hasError('email') ? 'Not a valid email' : '';
+}
 }
