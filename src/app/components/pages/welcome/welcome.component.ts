5147a6355c8c383970f0bdb4dd0a0705d9f87548
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-09-03 04:04:44 +0200

    changes timer to 3 seconds

--- src/app/components/pages/welcome/welcome.component.ts
+++ src/app/components/pages/welcome/welcome.component.ts
@@ -11,9 +11,9 @@ export class WelcomeComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-  //   setTimeout(() => {
-  //     this.router.navigate(['home']);
-  // }, 2000);
+    setTimeout(() => {
+      this.router.navigate(['home']);
+  }, 3000);
   }
 
 }
