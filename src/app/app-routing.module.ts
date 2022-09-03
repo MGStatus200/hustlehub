80697bcd81d354c6ee09fd08b3e4c46076b4673b
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-09-03 04:06:09 +0200

    new routes and default style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdvertiseComponent } from './components/pages/advertise/advertise.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 import { WelcomeComponent } from './components/pages/welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', component:WelcomeComponent },
   {path:'login',component:LoginComponent},
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'advertise',component:AdvertiseComponent}
 ];
 
 @NgModule({
