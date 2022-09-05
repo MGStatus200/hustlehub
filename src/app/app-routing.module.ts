8b5030cd22a33a48ac47472ceca1a95ee9fe7686
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-09-05 15:39:05 +0200

    event add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEventComponent } from './components/pages/add-event/add-event.component';
+import { AddServiceComponent } from './components/pages/add-service/add-service.component';
 import { AdvertiseComponent } from './components/pages/advertise/advertise.component';
 import { EventsInviteComponent } from './components/pages/events-invite/events-invite.component';
 import { HireServiceComponent } from './components/pages/hire-service/hire-service.component';
@@ -32,11 +34,13 @@ const routes: Routes = [
   {path:'esearch/:esearchTerm',component:EventsInviteComponent},
   {path:'ecategory/:ecategory',component:EventsInviteComponent},
   {path:'sEvent/:id',component:SingleEventComponent},
+  {path:'addEvent',component:AddEventComponent},
 
   {path:'hire',component:HireServiceComponent},
   {path:'hsearch/:hsearchTerm',component:HireServiceComponent},
   {path:'hcategory/:hcategory',component:HireServiceComponent},
   {path:'sService/:id',component:SingleServiceComponent},
+  {path:'addService',component:AddServiceComponent}
 
 
 
